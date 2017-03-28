7719b2d6e69aba98a7b867a88c7379377fc64d0b
edgracilla <ed.gracilla@gmail.com>
2017-03-28 17:27:09 +0800

    update

--- app.js
+++ app.js
@@ -1,74 +1,71 @@
-'use strict';
+'use strict'
 
-var uuid          = require('node-uuid'),
-	async         = require('async'),
-	isArray       = require('lodash.isarray'),
-	platform      = require('./platform'),
-	isPlainObject = require('lodash.isplainobject'),
-	database;
+const reekoh = require('reekoh')
+const plugin = new reekoh.plugins.Storage()
+
+const async = require('async')
+const uuid = require('node-uuid')
+const isPlainObject = require('lodash.isplainobject')
+
+let database = null
 
 let sendData = function (data, callback) {
-	if (!data._id) data._id = uuid.v4();
-	
-	database.insert(data, function (insertError) {
-		if (!insertError) {
-			platform.log(JSON.stringify({
-				title: 'Record Successfully inserted to CouchDB.',
-				data: data
-			}));
-		}
+  if (!data._id) data._id = uuid.v4()
 
-		callback(insertError);
-	});
-};
+  database.insert(data, function (insertError) {
+    if (!insertError) {
+      plugin.emit('processed')
+      plugin.log(JSON.stringify({
+        title: 'Record Successfully inserted to CouchDB.',
+        data: data
+      }))
+    }
 
-platform.on('data', function (data) {
-	if (isPlainObject(data)) {
-		sendData(data, (error) => {
-			if (error) platform.handleException(error);
-		});
-	}
-	else if (isArray(data)) {
-		async.each(data, (datum, done) => {
-			sendData(datum, done);
-		}, (error) => {
-			if (error) platform.handleException(error);
-		});
-	}
-	else
-		platform.handleException(new Error(`Invalid data received. Data must be a valid Array/JSON Object or a collection of objects. Data: ${data}`));
-});
+    callback(insertError)
+  })
+}
 
-/**
- * Emitted when the platform shuts down the plugin. The Storage should perform cleanup of the resources on this event.
- */
-platform.once('close', function () {
-	platform.notifyClose();
-});
+plugin.on('data', (data) => {
+  if (isPlainObject(data)) {
+    sendData(data, (error) => {
+      if (error) plugin.logException(error)
+    })
+  } else if (Array.isArray(data)) {
+    async.each(data, (datum, done) => {
+      sendData(datum, done)
+    }, (error) => {
+      if (error) plugin.logException(error)
+    })
+  } else {
+    plugin.logException(new Error(`Invalid data received. Data must be a valid Array/JSON Object or a collection of objects. Data: ${data}`))
+  }
+})
 
-/**
- * Emitted when the platform bootstraps the plugin. The plugin should listen once and execute its init process.
- * Afterwards, platform.notifyReady() should be called to notify the platform that the init process is done.
- * @param {object} options The options or configuration injected by the platform to the plugin.
- */
-platform.once('ready', function (options) {
-	let host = `${options.host}`, auth = '';
+plugin.once('ready', () => {
+  let options = plugin.config
+  let host = `${options.host}`
+  let auth = ''
 
-	if (options.port)
-		host = `${host}:${options.port}`;
+  if (options.port) {
+    host = `${host}:${options.port}`
+  }
 
-	if (options.user)
-		auth = `${options.user}`;
+  if (options.user) {
+    auth = `${options.user}`
+  }
 
-	if (options.password)
-		auth = `${auth}:${options.password}@`;
-	else if (options.user)
-		auth = `${auth}:@`;
+  if (options.password) {
+    auth = `${auth}:${options.password}@`
+  } else if (options.user) {
+    auth = `${auth}:@`
+  }
 
-	let nano = require('nano')(`${options.connection_type}://${auth}${host}`);
+  let nano = require('nano')(`${options.connectionType}://${auth}${host}`)
 
-	database = nano.use(options.database);
+  database = nano.use(options.database)
 
-	platform.notifyReady();
-	platform.log('CouchDB Storage has been initialized.');
-});
+  plugin.emit('init')
+  plugin.log('CouchDB Storage has been initialized.')
+})
+
+module.exports = plugin
--- test/couchdb-storage.test.js
+++ test/couchdb-storage.test.js
@@ -1,117 +1,92 @@
-/*
- * Just a sample code to test the storage plugin.
- * Kindly write your own unit tests for your own plugin.
- */
-'use strict';
+/* global describe, it, after, before */
+'use strict'
 
-var cp     = require('child_process'),
-	assert = require('assert'),
-	should = require('should'),
-	moment = require('moment'),
-	storage;
+const amqp = require('amqplib')
+const should = require('should')
+const moment = require('moment')
 
-var HOST            = '52.91.127.32',
-	USER            = 'rozzwalla',
-	PASSWORD        = 'reekoh',
-	PORT            = 5984,
-	CONNECTION_TYPE = 'http',
-	DATABASE        = 'reekoh_database',
-	ID              = new Date().getTime().toString();
+const ID = new Date().getTime().toString()
+const INPUT_PIPE = 'demo.pipe.storage'
+const BROKER = 'amqp://guest:guest@127.0.0.1/'
 
-var record = {
-	_id: ID,
-	co2: '11%',
-	temp: 23,
-	quality: 11.25,
-	reading_time: '2015-11-27T11:04:13.539Z',
-	metadata: {metadata_json: 'reekoh metadata json'},
-	random_data: 'abcdefg',
-	is_normal: true
-};
+let _app = null
+let _conn = null
+let _channel = null
 
-describe('Storage', function () {
-	this.slow(5000);
+let conf = {
+  host: 'localhost',
+  port: 5984,
+  user: 'rozzwalla',
+  password: 'reekoh',
+  connectionType: 'http',
+  database: 'reehkoh_database'
+}
 
-	after('terminate child process', function (done) {
-		this.timeout(7000);
+let record = {
+  _id: ID,
+  co2: '11%',
+  temp: 23,
+  quality: 11.25,
+  reading_time: '2015-11-27T11:04:13.539Z',
+  metadata: {metadata_json: 'reekoh metadata json'},
+  random_data: 'abcdefg',
+  is_normal: true
+}
 
-		storage.send({
-			type: 'close'
-		});
+describe('CouchDB Storage', () => {
+  before('init', () => {
+    process.env.BROKER = BROKER
+    process.env.INPUT_PIPE = INPUT_PIPE
+    process.env.CONFIG = JSON.stringify(conf)
 
-		setTimeout(function () {
-			storage.kill('SIGKILL');
-			done();
-		}, 5000);
-	});
+    amqp.connect(BROKER).then((conn) => {
+      _conn = conn
+      return conn.createChannel()
+    }).then((channel) => {
+      _channel = channel
+    }).catch((err) => {
+      console.log(err)
+    })
+  })
 
-	describe('#spawn', function () {
-		it('should spawn a child process', function () {
-			assert.ok(storage = cp.fork(process.cwd()), 'Child process not spawned.');
-		});
-	});
+  after('terminate', function () {
+    _conn.close()
+  })
 
-	describe('#handShake', function () {
-		it('should notify the parent process when ready within 5 seconds', function (done) {
-			this.timeout(5000);
+  describe('#start', function () {
+    it('should start the app', function (done) {
+      this.timeout(10000)
+      _app = require('../app')
+      _app.once('init', done)
+    })
+  })
 
-			storage.on('message', function (message) {
-				if (message.type === 'ready')
-					done();
-				else if (message.type === 'error') {
-					console.log(message.data);
-				}
-			});
+  describe('#data', function () {
+    it('should process the data', function (done) {
+      this.timeout(10000)
+      _channel.sendToQueue(INPUT_PIPE, new Buffer(JSON.stringify(record)))
+      _app.on('processed', done)
+    })
 
-			storage.send({
-				type: 'ready',
-				data: {
-					options: {
-						host: HOST,
-						port: PORT,
-						user: USER,
-						password: PASSWORD,
-						connection_type: CONNECTION_TYPE,
-						database: DATABASE
-					}
-				}
-			}, function (error) {
-				assert.ifError(error);
-			});
-		});
-	});
+    it('should have inserted the data', function (done) {
+      this.timeout(10000)
 
-	describe('#data', function () {
-		it('should process the data', function (done) {
-			storage.send({
-				type: 'data',
-				data: record
-			}, done);
-		});
-	});
+      let nano = require('nano')(conf.connectionType + '://' + conf.user + ':' + conf.password + '@' + conf.host + ':' + conf.port)
+      let database = nano.use(conf.database)
 
-	describe('#data', function () {
-		it('should have inserted the data', function (done) {
-			this.timeout(20000);
+      database.get(ID, {}, function (err, result) {
+        should.ifError(err)
 
+        should.equal(record.co2, result.co2, 'Data validation failed. Field: co2')
+        should.equal(record.temp, result.temp, 'Data validation failed. Field: temp')
+        should.equal(record.quality, result.quality, 'Data validation failed. Field: quality')
+        should.equal(record.random_data, result.random_data, 'Data validation failed. Field: random_data')
+        should.equal(moment(record.reading_time).format('YYYY-MM-DDTHH:mm:ss.SSSSZ'), moment(result.reading_time).format('YYYY-MM-DDTHH:mm:ss.SSSSZ'), 'Data validation failed. Field: reading_time')
+        should.equal(JSON.stringify(record.metadata), JSON.stringify(result.metadata), 'Data validation failed. Field: metadata')
+        should.equal(record.is_normal, result.is_normal, 'Data validation failed. Field: is_normal')
 
-			var nano     = require('nano')(CONNECTION_TYPE + '://' + USER + ':' + PASSWORD + '@' + HOST + ':' + PORT),
-				database = nano.use('reekoh_database');
-
-			database.get(ID, {}, function (err, result) {
-				should.equal(record.co2, result.co2, 'Data validation failed. Field: co2');
-				should.equal(record.temp, result.temp, 'Data validation failed. Field: temp');
-				should.equal(record.quality, result.quality, 'Data validation failed. Field: quality');
-				should.equal(record.random_data, result.random_data, 'Data validation failed. Field: random_data');
-				should.equal(moment(record.reading_time).format('YYYY-MM-DDTHH:mm:ss.SSSSZ'),
-					moment(result.reading_time).format('YYYY-MM-DDTHH:mm:ss.SSSSZ'), 'Data validation failed. Field: reading_time');
-				should.equal(JSON.stringify(record.metadata), JSON.stringify(result.metadata), 'Data validation failed. Field: metadata');
-				should.equal(record.is_normal, result.is_normal, 'Data validation failed. Field: is_normal');
-
-				done();
-			});
-
-
-		});
-	});
-});
+        done()
+      })
+    })
+  })
+})
